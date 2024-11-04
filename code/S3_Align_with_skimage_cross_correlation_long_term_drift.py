4fed745df2fd4e116282fe1031b25ebffa702b07
11mbs <146737852+11mbs@users.noreply.github.com>
2024-11-04 14:59:29 +0000

    Create separate scripts for jpeg data

--- code/S3_Align_with_skimage_cross_correlation_long_term_drift.py
+++ code/S3_Align_with_skimage_cross_correlation_long_term_drift.py
@@ -30,7 +30,6 @@ plot_nbr = 0
 
 # Get main path
 outer_dir = dirname(os.getcwd())
-#outer_dir = '/home/marcus1/Documents/data_collection/Data/drift_data (Copy)' ##REMOVE 
 # Ask user the path to the coordinate table (can be for a single protocol or all protocol merged)
 root = Tk()
 root.attributes("-topmost", True)
